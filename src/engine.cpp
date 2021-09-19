ea312c29db87b23e627bb7cbe67815823feea5c0
Harald Waxenegger <harald.waxenegger@gmail.com>
2021-09-19 19:31:41 +0200

    show toast for android if Vulkan not supported

--- src/engine.cpp
+++ src/engine.cpp
@@ -6,6 +6,12 @@ Engine::Engine(const std::string & appName, const std::string root, const uint32
 
     if(!this->graphics->isGraphicsActive()) {
         logError("Could not initialize Graphics Context");
+
+        #ifdef __ANDROID__
+                SDL_AndroidShowToast("Vulkan Not Supported", 1, 0, 0, 0);
+                std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+        #endif
+
         return;
     }
     
--- src/platform.cpp
+++ src/platform.cpp
@@ -6,8 +6,7 @@ int start(int argc, char* argv []) {
     const std::unique_ptr<Engine> engine = std::make_unique<Engine>("Sdl Vulkan App", argc > 1 ? argv[1] : "");
 
     engine->loadModels();
-    
-    
+
     // place a test object
     Component * rock = Components::INSTANCE()->addComponentFromModel("rock instance", "rock");
     rock->setPosition(0.0f,0.0f,-10.0f);
