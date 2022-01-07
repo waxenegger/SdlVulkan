48bbfc3acfc48b2d6117e58c2e1843d1f9b37ba0
Harald Waxenegger <harald.waxenegger@gmail.com>
2022-01-07 15:13:54 +0100

    added mipmaps

--- src/helper.cpp
+++ src/helper.cpp
@@ -3,14 +3,14 @@
 bool Helper::createImage(
     const VkPhysicalDevice & physicalDevice, const VkDevice & logicalDevice, 
     int32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, 
-    VkImage& image, VkDeviceMemory& imageMemory, uint16_t arrayLayers) {
+    VkImage& image, VkDeviceMemory& imageMemory, uint16_t arrayLayers, uint32_t mipLevels) {
         VkImageCreateInfo imageInfo{};
         imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
         imageInfo.imageType = VK_IMAGE_TYPE_2D;
         imageInfo.extent.width = width;
         imageInfo.extent.height = height;
         imageInfo.extent.depth = 1;
-        imageInfo.mipLevels = 1;
+        imageInfo.mipLevels = mipLevels;
         imageInfo.arrayLayers = arrayLayers;
         imageInfo.format = format;
         imageInfo.tiling = tiling;
@@ -53,7 +53,7 @@ bool Helper::createImage(
         return true;
 }
 
-VkImageView Helper::createImageView(const VkDevice & logicalDevice, VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, uint32_t layerCount) {
+VkImageView Helper::createImageView(const VkDevice & logicalDevice, VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, uint32_t layerCount, uint32_t mipLevels) {
     if (logicalDevice == nullptr) {
         logError("Renderer has not been initialized!");
         return nullptr;
@@ -70,7 +70,7 @@ VkImageView Helper::createImageView(const VkDevice & logicalDevice, VkImage imag
     viewInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
     viewInfo.subresourceRange.aspectMask = aspectFlags;
     viewInfo.subresourceRange.baseMipLevel = 0;
-    viewInfo.subresourceRange.levelCount = 1;
+    viewInfo.subresourceRange.levelCount = mipLevels;
     viewInfo.subresourceRange.baseArrayLayer = 0;
     viewInfo.subresourceRange.layerCount = layerCount;
 
@@ -134,10 +134,10 @@ bool Helper::createBuffer(
     return true;
 }
 
-VkCommandBuffer Helper::allocateCommandBuffer(const VkDevice & logicalDevice, const VkCommandPool & commandPool, const VkCommandBufferInheritanceInfo * cmdBufferInherit) {
+VkCommandBuffer Helper::allocateCommandBuffer(const VkDevice & logicalDevice, const VkCommandPool & commandPool) {
     VkCommandBufferAllocateInfo allocInfo{};
     allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-    allocInfo.level = cmdBufferInherit == nullptr ? VK_COMMAND_BUFFER_LEVEL_PRIMARY : VK_COMMAND_BUFFER_LEVEL_SECONDARY;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
     allocInfo.commandPool = commandPool;
     allocInfo.commandBufferCount = 1;
 
@@ -152,11 +152,11 @@ VkCommandBuffer Helper::allocateCommandBuffer(const VkDevice & logicalDevice, co
     return commandBuffer;
 }
 
-bool Helper::beginCommandBuffer(VkCommandBuffer & commandBuffer, const VkCommandBufferInheritanceInfo * cmdBufferInherit) {
+bool Helper::beginCommandBuffer(VkCommandBuffer & commandBuffer) {
     VkCommandBufferBeginInfo beginInfo{};
     beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-    beginInfo.flags = cmdBufferInherit == nullptr ? VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT : VK_COMMAND_BUFFER_USAGE_RENDER_PASS_CONTINUE_BIT;
-    beginInfo.pInheritanceInfo = cmdBufferInherit;
+    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+    beginInfo.pInheritanceInfo = VK_NULL_HANDLE;
 
     VkResult ret = vkBeginCommandBuffer(commandBuffer, &beginInfo);
     if (ret != VK_SUCCESS) {
@@ -168,11 +168,11 @@ bool Helper::beginCommandBuffer(VkCommandBuffer & commandBuffer, const VkCommand
 }
 
 
-VkCommandBuffer Helper::allocateAndBeginCommandBuffer(const VkDevice & logicalDevice, const VkCommandPool & commandPool, const VkCommandBufferInheritanceInfo * cmdBufferInherit) {
-    VkCommandBuffer commandBuffer = Helper::allocateCommandBuffer(logicalDevice, commandPool, cmdBufferInherit);
+VkCommandBuffer Helper::allocateAndBeginCommandBuffer(const VkDevice & logicalDevice, const VkCommandPool & commandPool) {
+    VkCommandBuffer commandBuffer = Helper::allocateCommandBuffer(logicalDevice, commandPool);
     if (commandBuffer == nullptr) return nullptr;
 
-    if (!Helper::beginCommandBuffer(commandBuffer, cmdBufferInherit)) {
+    if (!Helper::beginCommandBuffer(commandBuffer)) {
         vkFreeCommandBuffers(logicalDevice, commandPool, 1, &commandBuffer);
         return nullptr;
     };
@@ -301,7 +301,7 @@ void Helper::copyModelsContentIntoBuffer(void* data, ModelsContentType modelsCon
 
 bool Helper::transitionImageLayout(
     const VkDevice & logicalDevice, const VkCommandPool & commandPool, const VkQueue & graphicsQueue, 
-    VkImage image, VkImageLayout oldLayout, VkImageLayout newLayout, uint16_t layerCount) {
+    VkImage image, VkImageLayout oldLayout, VkImageLayout newLayout, uint16_t layerCount, uint32_t mipLevels) {
     
     VkCommandBuffer commandBuffer = Helper::allocateAndBeginCommandBuffer(logicalDevice, commandPool);
     if (commandBuffer == nullptr) return false;
@@ -315,7 +315,7 @@ bool Helper::transitionImageLayout(
     barrier.image = image;
     barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
     barrier.subresourceRange.baseMipLevel = 0;
-    barrier.subresourceRange.levelCount = 1;
+    barrier.subresourceRange.levelCount = mipLevels;
     barrier.subresourceRange.baseArrayLayer = 0;
     barrier.subresourceRange.layerCount = layerCount;
 
@@ -384,3 +384,81 @@ VkCommandPool Helper::createCommandPool(const VkDevice & logicalDevice, const ui
     return pool;
 }
 
+void  Helper::generateMipmaps(const VkDevice & logicalDevice, const VkCommandPool & commandPool, const VkQueue & graphicsQueue, const VkImage & image, const int32_t width, const int32_t height, const uint32_t levels) {
+    VkCommandBuffer commandBuffer = Helper::allocateAndBeginCommandBuffer(logicalDevice, commandPool);
+
+    VkImageMemoryBarrier barrier{};
+    barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
+    barrier.image = image;
+    barrier.srcQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
+    barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    barrier.subresourceRange.baseArrayLayer = 0;
+    barrier.subresourceRange.layerCount = 1;
+    barrier.subresourceRange.levelCount = 1;
+
+    int32_t mipWidth = width;
+    int32_t mipHeight = height;
+
+    for (uint32_t i = 1; i < levels; i++) {
+        barrier.subresourceRange.baseMipLevel = i - 1;
+        barrier.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        barrier.newLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
+        barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barrier.dstAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+
+        vkCmdPipelineBarrier(commandBuffer,
+            VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0,
+            0, nullptr,
+            0, nullptr,
+            1, &barrier);
+
+        VkImageBlit blit{};
+        blit.srcOffsets[0] = {0, 0, 0};
+        blit.srcOffsets[1] = {mipWidth, mipHeight, 1};
+        blit.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        blit.srcSubresource.mipLevel = i - 1;
+        blit.srcSubresource.baseArrayLayer = 0;
+        blit.srcSubresource.layerCount = 1;
+        blit.dstOffsets[0] = {0, 0, 0};
+        blit.dstOffsets[1] = { mipWidth > 1 ? mipWidth / 2 : 1, mipHeight > 1 ? mipHeight / 2 : 1, 1 };
+        blit.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+        blit.dstSubresource.mipLevel = i;
+        blit.dstSubresource.baseArrayLayer = 0;
+        blit.dstSubresource.layerCount = 1;
+
+        vkCmdBlitImage(commandBuffer,
+            image, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+            image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+            1, &blit,
+            VK_FILTER_LINEAR);
+
+        barrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
+        barrier.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+        barrier.srcAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
+        barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+        vkCmdPipelineBarrier(commandBuffer,
+            VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0,
+            0, nullptr,
+            0, nullptr,
+            1, &barrier);
+
+        if (mipWidth > 1) mipWidth /= 2;
+        if (mipHeight > 1) mipHeight /= 2;
+    }
+
+    barrier.subresourceRange.baseMipLevel = levels - 1;
+    barrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
+    barrier.newLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+    barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+    barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+
+    vkCmdPipelineBarrier(commandBuffer,
+        VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0,
+        0, nullptr,
+        0, nullptr,
+        1, &barrier);
+
+    Helper::endCommandBufferWithSubmit(logicalDevice, commandPool, graphicsQueue, commandBuffer);
+}
